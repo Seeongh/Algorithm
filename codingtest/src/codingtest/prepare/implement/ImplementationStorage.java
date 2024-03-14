9ad3e89186daf3d8c3bfc4fa7e7fbab9a37c2071
안성현 <mikstic7@gmail.com>
2024-03-15 00:08:10 +0900

    <Pratice>구현 연습하기 - index 기억하는 방식으로

--- codingtest/src/codingtest/prepare/implement/ImplementationStorage.java
+++ codingtest/src/codingtest/prepare/implement/ImplementationStorage.java
@@ -8,30 +8,44 @@ public class ImplementationStorage {
         int result_ud = 1;
         int result_lr = 1;
 
-        for(String p: strRLUD) {
-
-            if(p.equals("R")) {
-                if(result_lr < n) {
-                    result_lr+=1;
-                }
-            }
-            else if(p.equals("L")) {
-                if(result_lr > 1) {
-                    result_lr -= 1;
-                }
-            }
-            else if(p.equals("U")) {
-                if(result_ud > 1) {
-                    result_ud -= 1;
-                }
-            }
-            else if(p.equals("D")) {
-                if(result_ud < n) {
-                    result_ud += 1;
+        int[] arry= {0,0,-1,1};
+        int[] arrx = {-1,1,0,0};
+        String[] move_types = {"L","R","U","D"};
+        for(String m : strRLUD) {
+            for(int i =0 ; i<move_types.length; i++){
+                if(m.equals(move_types[i])) {
+                    result_lr = (result_lr + arrx[i]) > 0? (result_lr + arrx[i]): result_lr;
+                    result_ud = (result_ud + arry[i]) > 0? (result_ud + arry[i]): result_ud;
                 }
             }
         }
 
+
+//
+//        for(String p: strRLUD) {
+//
+//            if(p.equals("R")) {
+//                if(result_lr < n) {
+//                    result_lr+=1;
+//                }
+//            }
+//            else if(p.equals("L")) {
+//                if(result_lr > 1) {
+//                    result_lr -= 1;
+//                }
+//            }
+//            else if(p.equals("U")) {
+//                if(result_ud > 1) {
+//                    result_ud -= 1;
+//                }
+//            }
+//            else if(p.equals("D")) {
+//                if(result_ud < n) {
+//                    result_ud += 1;
+//                }
+//            }
+//        }
+//
         System.out.println( result_ud+ " " + result_lr );
     }
 }
