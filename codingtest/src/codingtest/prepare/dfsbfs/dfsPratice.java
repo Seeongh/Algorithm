74aafbb03311339d09e14e4e344431d88840ea04
안성현 <mikstic7@gmail.com>
2024-04-01 23:20:31 +0900

    <Pratice>dfs 재귀 연습

--- codingtest/src/codingtest/prepare/dfsbfs/dfsPratice.java
+++ codingtest/src/codingtest/prepare/dfsbfs/dfsPratice.java
@@ -5,64 +5,31 @@ import java.util.Arrays;
 
 public class dfsPratice {
 
+    static int answer;
+    static int target;
+    static int[] numbers;
 
-    static boolean[] chk;
-    static int len = 0 ;
-    static int sum = 0;
     public static void main(String[] args) {
-        int answer = 0;
-        int[] numbers = {1,1,1,1,1};
-        //int[] numbers = {4,1,2,1};
-        int target = 3;
-        len= numbers.length;
-        chk = new boolean[len];
+        //int[] numbers = {1,1,1,1,1};
+        numbers = new int[]{4, 1, 2, 1};
+        //int[] numbers= {3,4,2,1};
+        target = 4;
 
-
-        sum = Arrays.stream(numbers).sum();
-
-        if(sum > target) {
-            for(int i =0 ;i <len; i++) {
-                for(int j =i ; j<len ; j++) {
-                    if(!chk[i]) {
-
-                        answer += getSum(numbers[i], numbers, target, i,j);
-                        System.out.println("answer : " + answer);
-                    }
-                }
-            }
-        }
-        else if(sum ==target) {
-            System.out.println(1);
-        }
-
+       dfs(0, 0);
         System.out.println(answer);
     }
 
-    public static int getSum(int minusSum, int[] numbers, int target, int ti, int tj) {
+    public static void dfs(int index, int sum) {
 
-         if(tj == len) {
-            return 0 ;
+
+        if(index == numbers.length) {
+            if(target == sum ) {
+                answer++;
+            }
+            return;
         }
 
-         int tmpSum = 0;
-
-         tmpSum = sum -minusSum-numbers[tj];
-
-
-        System.out.println("tmpSum = " + tmpSum + " minusSum = " + minusSum + " i : " + ti + " j :" + tj);
-        //minusSum += numbers[tj];
-        if(tmpSum == target) {
-            chk[ti] = true;
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            return 1;
-        }
-        else if(tmpSum> target){
-            return getSum( tmpSum,numbers,target,ti,tj+1);
-        }
-        else { //한번 더 빼기를 해야하는 상황
-
-            return 0 ;
-        }
-
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index+1, sum - numbers[index]);
     }
 }
