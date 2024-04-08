3bf6c659f49f6e4e7b17cc5b92445d283e34b08a
안성현 <mikstic7@gmail.com>
2024-04-08 23:46:20 +0900

    <Pratice>dfs,bfs 단어 변경하기

---
+++ codingtest/src/codingtest/prepare/dfsbfs/dfsbfsPratice.java
@@ -0,0 +1,15 @@
+package codingtest.prepare.dfsbfs;
+
+//글자 바꾸기
+public class dfsbfsPratice {
+
+    String start = "hit";
+    String target = "cog";
+    String[] words = {"hot", "dot","dog","lot","log","cog"};
+    public static void main(String[] args) {
+        //start -> target으로 단어 변경하기
+
+        
+
+    }
+}
