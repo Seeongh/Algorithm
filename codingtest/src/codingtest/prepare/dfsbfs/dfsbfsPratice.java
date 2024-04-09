ead39f8f11d51fabf7083343ff19a916157e6220
안성현 <mikstic7@gmail.com>
2024-04-09 23:44:57 +0900

    <Pratice>dfs,bfs 단어 변경하기 구현

--- codingtest/src/codingtest/prepare/dfsbfs/dfsbfsPratice.java
+++ codingtest/src/codingtest/prepare/dfsbfs/dfsbfsPratice.java
@@ -3,13 +3,27 @@ package codingtest.prepare.dfsbfs;
 //글자 바꾸기
 public class dfsbfsPratice {
 
-    String start = "hit";
-    String target = "cog";
-    String[] words = {"hot", "dot","dog","lot","log","cog"};
+    static String start = "hit";
+    static String target = "cog";
+    static int n;
+    static String[] words = {"hot", "dot","dog","lot","log","cog"};
+
     public static void main(String[] args) {
         //start -> target으로 단어 변경하기
 
-        
+        n = start.length();
+        for(int i =0 ; i < n ; i++) {
+            if(start.charAt(i) != target.charAt(i)) {
+                dfs(i);
+            }
+        }
 
+    }
+
+    public static void dfs(int i) {
+
+        for(int j = 0 ; j < words.length; j++) {
+            if(words[i].charAt(i) )
+        }
     }
 }
