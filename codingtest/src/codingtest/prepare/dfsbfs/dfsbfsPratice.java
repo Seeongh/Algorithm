10dc64dca25c004870fff3a1d03177272410c3f8
안성현 <mikstic7@gmail.com>
2024-04-13 23:56:35 +0900

    <Pratcie>dfs,bfs - 경로 설정하기 연습

--- codingtest/src/codingtest/prepare/dfsbfs/dfsbfsPratice.java
+++ codingtest/src/codingtest/prepare/dfsbfs/dfsbfsPratice.java
@@ -3,87 +3,58 @@ package codingtest.prepare.dfsbfs;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 //글자 바꾸기
 public class dfsbfsPratice {
 
     static String start = "hit";
     static String target = "cog";
-    static int n;
+    static int n,m, result;
    static String[] words = {"hot", "dot","dog","lot","log","cog"};
     //static String[] words = {"hot", "dot","dog","lot","log"};
 
     static String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
     static String smallStr= "" ;
-    static boolean[] chk= null;
+    static boolean[][] chk= null;
+    static Stack<realnodes> stack = null;
+    static String[] answer = null;
+
     public static void main(String[] args) {
         //start -> target으로 단어 변경하기
-        int n = tickets.length;
-        int m = 2;
-        int cnt = 0; int result =0 ;
-        String[] answer = new String[n+1];
-       chk= new boolean[n];
-        String tmp="";
+         n = tickets.length;
+         m = 2;
+        chk = new boolean[n][n+1];
+        stack= new Stack<>();
+        answer = new String[n+1];
 
-        Queue<String> q = new LinkedList<>();
-        q.offer("ICN"); //INC
-
-//        while(!q.isEmpty()){
-//            tmp = q.poll();
-//            System.out.println("cnt: " + cnt);
-//            answer[cnt++] = tmp;
-//            for(int i =0 ;i < n; i++) {
-//                if(!chk[i] && tmp.equals(tickets[i][0])) {
-//
-//                    if(dfs3(tickets[i][1],0)){
-//                        result = i;
-//                    }
-//                }
-//            }
-//
-//            q.offer(smallStr);
-//            System.out.println(smallStr);
-//            chk[result]=true;
-//            smallStr = "";
-//
-//        }
-
-        smallStr = "ICN";
-        while(cnt <= n){
-            //tmp = q.poll();
-            answer[cnt++] = smallStr;
-
-            smallStr = "";
-
-            for(int i =0 ;i < n; i++) {
-                if(!chk[i] && tmp.equals(tickets[i][0])) {
-                    if(dfs3(tickets[i][1],0)){
-                        result = i;
-                        smallStr = tickets[i][1];
-                        System.out.println(smallStr);
-                    }
-                }
-            }
-            // answer[cnt++] = tmp;
-            //q.offer(smallStr);
-            chk[result]=true;
-
+        stack.push(new realnodes("ICN", -1));
+        for(int a =0 ; a < n ; a++) {
+            dfs3( "ICN" ,a,0);
         }
-      //  Arrays.stream(answer)
-       //         .forEach(i -> System.out.println(i));
+
+        while(!stack.empty()){
+            System.out.println(stack.pop().city);
+        }
     }
-    public static boolean dfs3(String compareStr, int index){
-        if(index < 3)  {
-            if(smallStr.equals("")|| (smallStr.charAt(index) > compareStr.charAt(index) )){
-               
-                return true;
-            }
-            else{
-                return dfs3(compareStr, index+1);
-            }
+    public static boolean dfs3(String city,int check, int i){
+        //종료 조건
+       if(stack.size() / n+1 == 0) {
+           return true;
+       }
+        if( i == n ) { //다 돌았는데 못찾음: 경로가 아님 || stack 이 다 찬경우
+            return false ;
         }
 
-        return false;
+        if(!chk[check][i] && city.equals(tickets[i][0])) {
+            stack.push(new realnodes(tickets[i][1], i));
+            city = tickets[i][1];
+            chk[check][i] = true;
+            System.out.println("stack push city: " + city +", " + i);
+            return dfs3(city, check,0);
+        }
+        return  dfs3(city, check,i + 1);
+
     }
 
     // 알파벳 start-> target으로 바꾸기
@@ -145,7 +116,15 @@ public class dfsbfsPratice {
 
     }
 }
+class realnodes {
+    String city;
+    int order;
 
+    public realnodes(String city, int order) {
+        this.city = city;
+        this.order = order;
+    }
+}
 class nodes{
 
     String pString;
