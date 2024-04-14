96bafed139d0a9ee9865ca4686c3208f81beb6fa
안성현 <mikstic7@gmail.com>
2024-04-14 23:17:13 +0900

    <Pratice>dfs,bfs - 여행가기 연습(완)

--- codingtest/src/codingtest/prepare/dfsbfs/dfsbfsPratice.java
+++ codingtest/src/codingtest/prepare/dfsbfs/dfsbfsPratice.java
@@ -1,9 +1,6 @@
 package codingtest.prepare.dfsbfs;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 //글자 바꾸기
 public class dfsbfsPratice {
@@ -16,44 +13,75 @@ public class dfsbfsPratice {
 
     static String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
     static String smallStr= "" ;
-    static boolean[][] chk= null;
-    static Stack<realnodes> stack = null;
+    static boolean[] chk= null;
+    static Stack<String> stack = null;
     static String[] answer = null;
+    static ArrayList<Stack<String>> arr = new ArrayList<>();
 
     public static void main(String[] args) {
         //start -> target으로 단어 변경하기
          n = tickets.length;
          m = 2;
-        chk = new boolean[n][n+1];
-        stack= new Stack<>();
-        answer = new String[n+1];
+        chk = new boolean[n];
+        Stack<String> stack = new Stack<>();
+        stack.push("ICN");
+        dfs3("ICN",stack,0 );
 
-        stack.push(new realnodes("ICN", -1));
-        for(int a =0 ; a < n ; a++) {
-            dfs3( "ICN" ,a,0);
+
+        if(arr.size()> 1) {
+            Comparator<Stack<String>> Alphabet = new Comparator<Stack<String>>() {
+                @Override
+                public int compare(Stack s1, Stack s2) {
+                    for(int a = 0 ; a< 3; a++) {
+                        if(!s1.get(a).equals(s2.get(a))) {
+                            String o1 = (String) s1.get(a);
+                            String o2= (String) s2.get(a);
+                            System.out.println("s1 a, 알파벳: " + a+", " + s1.get(a));
+                            System.out.println("s2 a, 알파벳: " + a+", " + s2.get(a));
+                            return o1.compareTo(o2); //음수 반환시 앞에게 뒤에거보다 앞
+                        }
+                    }
+
+                    return 0;
+                }
+            };
+
+           Collections.sort(arr, Alphabet);
+
         }
 
-        while(!stack.empty()){
-            System.out.println(stack.pop().city);
-        }
+
+
+
+        // 길이로 정렬하는 Comparator 구현
+
     }
-    public static boolean dfs3(String city,int check, int i){
+    public static void dfs3(String city, Stack<String> st, int i){
         //종료 조건
-       if(stack.size() / n+1 == 0) {
-           return true;
-       }
-        if( i == n ) { //다 돌았는데 못찾음: 경로가 아님 || stack 이 다 찬경우
-            return false ;
+        if(i == n) {
+            Stack<String> st2 = new Stack<>();
+           for(String s: st) {
+               st2.push(s);
+           }
+            arr.add(st2);
+            return ;
         }
 
-        if(!chk[check][i] && city.equals(tickets[i][0])) {
-            stack.push(new realnodes(tickets[i][1], i));
-            city = tickets[i][1];
-            chk[check][i] = true;
-            System.out.println("stack push city: " + city +", " + i);
-            return dfs3(city, check,0);
+        for(int j =0 ;j < n; j ++) {
+            if(!chk[j] && city.equals(tickets[j][0])) {
+
+                System.out.println(city+", " + tickets[j][1]+", (" + i+")");
+                st.push(tickets[j][1]);
+                chk[j] = true;
+
+                dfs3(tickets[j][1], st,i+1);
+
+                chk[j] = false;
+                st.pop();
+
+            }
         }
-        return  dfs3(city, check,i + 1);
+
 
     }
 
