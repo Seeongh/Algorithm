1b323b7c0bfa7a7501cfaf89efd335950cf6821b
안성현 <mikstic7@gmail.com>
2024-04-18 23:53:52 +0900

    <Pratice>dfs - 최단 경로 구하기(game)

---
+++ codingtest/src/codingtest/prepare/dfsbfs/DFSBFS_GAME.java
@@ -0,0 +1,10 @@
+package codingtest.prepare.dfsbfs;
+
+public class DFSBFS_GAME {
+    public static void main(String[] args) {
+        //가장 짧은 경로 구하기
+        int[][] rectangle = {{1,1,7,4},{3,2,5,5},{4,3,6,9},{2,6,8,8}};
+        int characterX =1, characterY = 3; //캐릭터 위치
+        int itemX = 7, itemY =8 ; //목표 위치
+    }
+}
