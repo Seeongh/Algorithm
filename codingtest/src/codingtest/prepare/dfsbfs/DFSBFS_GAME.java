6f53d4bc67fdca43384aa47dbeb3b1c834852405
안성현 <mikstic7@gmail.com>
2024-04-29 23:35:21 +0900

    <Pratice>dfs,bfs - 게임 최단경로 구하기

--- codingtest/src/codingtest/prepare/dfsbfs/DFSBFS_GAME.java
+++ codingtest/src/codingtest/prepare/dfsbfs/DFSBFS_GAME.java
@@ -8,77 +8,65 @@ public class DFSBFS_GAME {
     static int[][] rectangle = {{1, 1, 7, 4}, {3, 2, 5, 5}, {4, 3, 6, 9}, {2, 6, 8, 8}};
     static int characterX = 1, characterY = 3; //캐릭터 위치
     static int itemX = 7, itemY = 8; //목표 위치
+    static int arr[][];
+    static int answer =0 ;
+    static int X =0 ;
+    static int Y =0 ;
+
+    static int[] _x = {-1, 1, 0, 0};
+    static int[] _y = {0, 0, -1, 1};
     public static void main(String[] args) {
         //가장 짧은 경로 구하기
-        int len  =0 ;
-        //제일 큰수를 구해서 chk의 배열 길이로씀
-        for(int i =0; i < rectangle.length; i++) {
-            for(int j = 0; j < rectangle.length; j++){
-                if(rectangle[i][j] > len) len = rectangle[i][j];
+        arr = new int[101][101];
+        answer = 99999;
+        X = itemX*2;
+        Y = itemY*2;
+
+        for(int r=0; r<rectangle.length; r++){
+            int x1 = rectangle[r][0]*2, y1 = rectangle[r][1]*2, x2 = rectangle[r][2]*2, y2 = rectangle[r][3]*2;
+
+            for(int i=x1; i<=x2; i++){
+                arr[i][y1] = 1;
+                arr[i][y2] = 1;
+            }
+            for(int j=y1; j<=y2; j++){
+                arr[x1][j] = 1;
+                arr[x2][j] = 1;
             }
         }
 
-        int[][] chk = new int[len+1][len+1];
-
-        int[] dx = {-1, 0, 1, 0}; //x= 좌우
-        int[] dy = {0, 1, 0, -1}; //y= 위아래
-        Queue<gameNode> q = new LinkedList<>();
-        q.offer(new gameNode(characterX, characterY));
-        gameNode prevNode = null;
-        while (!q.isEmpty()) {
-            gameNode tmp = q.poll();
-
-            for (int a = 0; a < 4; a++) { //위아래양옆 4번
-                int nx = tmp.i + dx[a];
-                int ny = tmp.j + dy[a];
-
-                if(nx<0||ny<0||nx>len || ny > len) continue; //범위를 벗어나면 안됨
-
-                if(findRange(nx, ny) > 0) {
-                    q.offer(new gameNode(nx,ny));
-                    chk[nx][ny] = chk[tmp.i][tmp.j] +1;
+        for(int i=2; i<101; i++){
+            for(int j=2; j<101; j++){
+                if(arr[i][j]==0){ continue; }
+                for(int r=0; r<rectangle.length; r++){
+                    int x1 = rectangle[r][0]*2, y1 = rectangle[r][1]*2, x2 = rectangle[r][2]*2, y2 = rectangle[r][3]*2;
+                    if(x1<i&&i<x2&&y1<j&&j<y2){ arr[i][j] = 0; }
                 }
             }
-            prevNode = new gameNode(tmp.i, tmp.j);
         }
+        findRange(characterX*2, characterY*2, 0);
 
-        for(int i =0 ; i < chk.length ; i++) {
-            System.out.println("");
-            for(int j = 0 ;j <chk.length; j++) {
-                System.out.print(chk[i][j]+" " );
-            }
-        }
-
-        System.out.println(chk[itemX][itemY]);
+        System.out.println(answer/2);
     }
 
 
 
-    public static int findRange(int x, int y) {
 
-        int[] tmp;
-        int result = 0;
-        for(int i =0 ;i <rectangle.length; i++) {
-            tmp = rectangle[i];
-            if( x < tmp[0] ||  x > tmp[2] || y <tmp[1] || y > tmp[3]) continue; //범위에도 안듦
-            else{ //범위에는 듦
-                if ((x == tmp[0]) ||
-                    (x == tmp[2]) ||
-                    (y == tmp[1]) ||
-                    (y == tmp[3])) { //모서리
-                    System.out.println(" "+ x+", " +y);
-                    result++;
-                }
-                else{ //영역
-                    result = 0;
-                    break;
-                }
+    public static void findRange(int x, int y, int go){
+        if(x==X&&y==Y){
+            if(go<answer){ answer = go; }
+            return;
+        }
+        arr[x][y] = 0;
+        for(int v=0; v<4; v++){
+            int n_x = x+_x[v], n_y = y+_y[v];
+            if(1<n_x&&n_x<101&&1<n_y&&n_y<101&&arr[n_x][n_y]==1){
+                findRange(n_x, n_y, go+1);
             }
         }
-
-        return result;
-
+        arr[x][y] = 1;
     }
+
 }
 
 
