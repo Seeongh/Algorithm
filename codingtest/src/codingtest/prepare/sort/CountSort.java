05096d7c33760bd1763e1bce43c8ffdd1b310ef3
안성현 <mikstic7@gmail.com>
2024-07-29 23:57:43 +0900

    <study> 계수 정렬 공부

---
+++ codingtest/src/codingtest/prepare/sort/CountSort.java
@@ -0,0 +1,27 @@
+package codingtest.prepare.sort;
+
+import java.util.Arrays;
+
+/**
+ * 개수 정렬
+ * 특정 조건이 부합할때 사용함
+ * -> 모든 조건을 담을 수 있는 크기의 리스트(배열)을 선언해야함
+ * 데이터를 하나씩 확인하며 데이터 값과 동일한 인덱스의 데이터를 1씩 증가
+ * 횟수만큼 출력
+ */
+public class CountSort {
+
+    static int[] arr = {7,5,9,0,3,1,6,2,9,1,4,8,0,5,2};
+
+    public static void main(String[] args) {
+
+        int maxNum =  Arrays.stream(arr).max().getAsInt(); //최대값 구하기
+        int[] indArr = new int[maxNum+1];
+
+        Arrays.stream(arr).forEach(i -> indArr[i] = indArr[i]+1);
+
+        Arrays.stream(indArr).forEach((ind,var)-> {});
+
+    }
+
+}
