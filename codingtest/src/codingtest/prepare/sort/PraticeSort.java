33a6a707ee39140a2d55b8444bea21b0550b0796
안성현 <mikstic7@gmail.com>
2024-08-19 23:55:30 +0900

    <study> 정렬 연습문제- 내림차순정렬

---
+++ codingtest/src/codingtest/prepare/sort/PraticeSort.java
@@ -0,0 +1,33 @@
+package codingtest.prepare.sort;
+
+import java.util.Arrays;
+
+/**
+ * 정렬 실전 문제(1)
+ * 위에서 아래로
+ *
+ * 수를 큰수부터 작은 수 순서로 내림정렬
+ */
+public class PraticeSort {
+    static int[] arr = {27,15,12};
+
+    static int maxlen = arr.length;
+    static int n = 3;
+
+    public static void main(String[] args) {
+        int tmp = 0;
+        Arrays.stream(arr).forEach(
+                (index) -> {
+                    for(int i =0 ; i<maxlen; i++) {
+
+                        if(arr[index] > arr[index+i]) {
+                            tmp = arr[index+i];
+                            arr[index+i] = arr[index];
+                            arr[index] = tmp;
+                        }
+
+                    }
+                }
+        );
+    }
+}
